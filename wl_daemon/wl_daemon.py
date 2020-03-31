59dfca29e4beda5005ad41448d6c5f85dfcb4556
nkostoulas <nkostoulas@gmail.com>
2020-03-31 15:44:05 +0100

    Aws config from env

--- wl_daemon/wl_daemon.py
+++ wl_daemon/wl_daemon.py
@@ -16,12 +16,7 @@ def parse_args():
     parser.add_argument('--rpcpassword', required=True, type=str, help="RPC password for client")
     parser.add_argument('--kyc_indir', required=True, type=str, help="Dir containing kycfiles to be onboarded")
     parser.add_argument('--kyc_toblacklistdir', required=True, type=str, help="Dir containing kycfiles to be blacklisted")
-    parser.add_argument('--init_store', required=False, type=bool, default=False, help="Initialize address storage")
-    parser.add_argument('--aws_key_id', required=True, type=str, help="Aws access key id")
-    parser.add_argument('--aws_secret_key', required=True, type=str, help="Aws secret access key")
-    parser.add_argument('--aws_region', required=True, type=str, help="Aws region name")
-    parser.add_argument('--aws_db_table', required=True, type=str, help="Aws db table name")
-
+    parser.add_argument('--init_store', action='store_true', help="Initialize address storage")
     return parser.parse_args()
 
 def main():
@@ -40,10 +35,10 @@ def main():
     conf["kyc_indir"] = args.kyc_indir
     conf["kyc_toblacklistdir"] = args.kyc_toblacklistdir
     conf["init_store"] = args.init_store
-    conf["aws_key_id"] = args.aws_key_id
-    conf["aws_secret_key"] = args.aws_secret_key
-    conf["aws_db_table"] = args.aws_db_table
-    conf["aws_region"] = args.aws_region
+    conf["aws_key_id"] = os.environ['AWS_ACCESS_KEY_ID']
+    conf["aws_secret_key"] = os.environ['AWS_SECRET_ACCESS_KEY']
+    conf["aws_db_table"] = os.environ['AWS_DYNAMO_TABLE']
+    conf["aws_region"] = os.environ['AWS_DEFAULT_REGION']
 
     wl_daemon = Whitelisting(conf)
     wl_daemon.start()
