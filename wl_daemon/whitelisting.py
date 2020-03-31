e86c3f3a2ec10e32743db855f687ee001151b041
nkostoulas <nkostoulas@gmail.com>
2020-03-31 14:03:10 +0100

    Store applicant addresses to aws dynamo db

--- wl_daemon/whitelisting.py
+++ wl_daemon/whitelisting.py
@@ -6,9 +6,19 @@ from .daemon import DaemonThread
 from .test_framework.authproxy import JSONRPCException
 from .connectivity import getoceand
 import hashlib
+import boto3
 
 BLOCK_TIME_DEFAULT = 60
 TRANSACTION_LIMIT = 1000
+
+def get_id_from_kycfile(kycfile):
+    f = open(kycfile, "r")
+    line = f.readline()
+    if "applicant-id" in line:
+        parts = line.split(":")
+        if len(parts) == 2:
+            return parts[1].rstrip().lstrip()
+    return ""
 
 class Whitelisting(DaemonThread):
     def __init__(self, conf):
@@ -20,7 +30,13 @@ class Whitelisting(DaemonThread):
         self.init_status()
         self.previous_height = -1
         self.FILEBLOCKSIZE = 65536
-
+        dynamo = boto3.resource(
+            'dynamodb',
+            aws_access_key_id=conf["aws_key_id"],
+            aws_secret_access_key=conf["aws_secret_key"],
+            region_name=conf["aws_region"]
+        )
+        self.db = dynamo.Table(conf["aws_db_table"])
 
     def init_status(self):
         self.towhitelist=set()
@@ -46,7 +62,7 @@ class Whitelisting(DaemonThread):
         if bNonePending == False:
             self.logger.warning("update_pendingtx: {} pending".format(nPending))
         return bNonePending
-        
+
     def update_files(self):
         self.towhitelist=set()
         self.logger.info("searching {} for kycfiles".format(self.conf["kyc_indir"]))
@@ -82,8 +98,44 @@ class Whitelisting(DaemonThread):
                     if not h in self.blacklisted:
                         self.toblacklist.add(h)
 
+    def init_store(self):
+        self.logger.info("Initializing address storage...")
+        for h in self.towhitelist:
+            f=self.path_dict[h]
+            self.save_kycfile_addrs(f)
+        for h in self.toblacklist:
+            f=self.path_dict[h]
+            self.delete_kycfile_addrs(f)
 
-        
+    def save_kycfile_addrs(self, kycfile):
+        try:
+            applicant_id = get_id_from_kycfile(kycfile)
+            self.logger.info("Saving addresses for applicant: {}".format(applicant_id))
+            _,addrs=self.ocean.validatekycfile(kycfile, True)
+            for addr in addrs:
+                resp = self.db.put_item(
+                   Item={
+                        'applicant_id': applicant_id,
+                        'address': addr
+                    }
+                )
+        except Exception as e:
+            self.logger.warning("Error saving kycfile addrs{}: {}".format(kycfile, e))
+
+    def delete_kycfile_addrs(self, kycfile):
+        try:
+            applicant_id = get_id_from_kycfile(kycfile)
+            self.logger.info("Deleting addresses for applicant: {}".format(applicant_id))
+            _,addrs=self.ocean.validatekycfile(kycfile, True)
+            for addr in addrs:
+                resp = self.db.delete_item(
+                   Key={
+                        'address': addr
+                    }
+                )
+        except Exception as e:
+            self.logger.warning("Error removing kycfile addrs{}: {}".format(kycfile, e))
+
     def update_status(self):
         self.logger.info("Updating status...")
         tmp=set()
@@ -92,24 +144,23 @@ class Whitelisting(DaemonThread):
             if self.is_whitelisted(f):
                 self.whitelisted.add(h)
                 tmp.add(h)
-            
+
         self.towhitelist=self.towhitelist.difference(tmp)
         self.blacklisted=self.blacklisted.difference(tmp)
-        
+
         tmp=set()
         for h in self.toblacklist:
             f=self.path_dict[h]
             if not self.is_whitelisted(f):
                 self.blacklisted.add(h)
                 tmp.add(h)
-                       
+
         self.toblacklist=self.toblacklist.difference(tmp)
         self.whitelisted=self.whitelisted.difference(tmp)
         #toblacklist overrides towhitelist
         self.towhitelist=self.towhitelist.difference(self.toblacklist)
         self.logger.info("...finished updating status.")
-        
-        
+
     def is_whitelisted(self, p):
         try:
             bResult=self.ocean.validatekycfile(p)["iswhitelisted"]
@@ -117,8 +168,7 @@ class Whitelisting(DaemonThread):
         except Exception as e:
             self.logger.warning("Error validating kycfile{}: {}".format(p, e))
             return False
-        
-                
+
     def run(self):
         self.logger.info("Daemon started")
         while not self.stopped():
@@ -131,6 +181,10 @@ class Whitelisting(DaemonThread):
             if self.update_pendingtx() != True:
                 continue
             self.update_files()
+            if self.conf["init_store"]:
+                self.init_store()
+                self.stop()
+                break
             self.update_status()
             self.onboard_kycfiles()
             self.blacklist_kycfiles()
@@ -145,7 +199,7 @@ class Whitelisting(DaemonThread):
                 self.ocean = getoceand(self.conf)
         self.logger.error("Failed reconnecting to client")
         self.stop()
-            
+
     def get_blockcount(self):
         return self.rpc_retry(self.ocean.getblockcount)
 
@@ -158,6 +212,7 @@ class Whitelisting(DaemonThread):
             try:
                 f=self.path_dict[h]
                 txid=self.onboard_kycfile(f)
+                self.save_kycfile_addrs(f)
                 self.pendingtx.add(txid)
             except Exception as e:
                 self.logger.error(e)
@@ -168,16 +223,17 @@ class Whitelisting(DaemonThread):
                         if 'No whitelist asset available' in e.error['message']:
                             break
                 continue
-        
+
     def blacklist_kycfile(self, kycfile):
         return self.ocean.blacklistuser(kycfile)
-                
+
     def blacklist_kycfiles(self):
         self.logger.info("blacklisting {} files".format(len(self.toblacklist)))
         for h in self.toblacklist:
             try:
                 f=self.path_dict[h]
                 txid=self.blacklist_kycfile(f)
+                self.delete_kycfile_addrs(f)
                 self.pendingtx.add(txid)
             except Exception as e:
                 self.logger.error(e)
@@ -188,8 +244,3 @@ class Whitelisting(DaemonThread):
                         if 'No whitelist asset available' in e.error['message']:
                             break
                 continue
-
-
-                    
-    
-
--- wl_daemon/wl_daemon.py
+++ wl_daemon/wl_daemon.py
@@ -16,6 +16,12 @@ def parse_args():
     parser.add_argument('--rpcpassword', required=True, type=str, help="RPC password for client")
     parser.add_argument('--kyc_indir', required=True, type=str, help="Dir containing kycfiles to be onboarded")
     parser.add_argument('--kyc_toblacklistdir', required=True, type=str, help="Dir containing kycfiles to be blacklisted")
+    parser.add_argument('--init_store', required=False, type=bool, default=False, help="Initialize address storage")
+    parser.add_argument('--aws_key_id', required=True, type=str, help="Aws access key id")
+    parser.add_argument('--aws_secret_key', required=True, type=str, help="Aws secret access key")
+    parser.add_argument('--aws_region', required=True, type=str, help="Aws region name")
+    parser.add_argument('--aws_db_table', required=True, type=str, help="Aws db table name")
+
     return parser.parse_args()
 
 def main():
@@ -33,8 +39,12 @@ def main():
     conf["rpcconnect"] = args.rpconnect
     conf["kyc_indir"] = args.kyc_indir
     conf["kyc_toblacklistdir"] = args.kyc_toblacklistdir
-    
-    
+    conf["init_store"] = args.init_store
+    conf["aws_key_id"] = args.aws_key_id
+    conf["aws_secret_key"] = args.aws_secret_key
+    conf["aws_db_table"] = args.aws_db_table
+    conf["aws_region"] = args.aws_region
+
     wl_daemon = Whitelisting(conf)
     wl_daemon.start()
 
